51fd2d83b4a3e3a39db11056d061106d73503c17
Wryxo <wryxsk@gmail.com>
2014-02-02 21:42:02 +0100

    Source added
    I added source code for application

---
+++ Administration/Administration/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Administration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Listen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop Listen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(154, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(581, 484);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(136, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Testing";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(12, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 37);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Install";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(36, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "C:";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(111, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(37, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "D:";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 220);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(136, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 508);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Administration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Administration/Administration/Form1.cs
@@ -0,0 +1,351 @@
+﻿using DifferenceEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Management;
+using System.Reflection;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Administration
+{
+    public partial class Form1 : Form
+    {
+        FileSystemWatcher watcher;
+        FileSystemWatcher watcher2;
+        System.IO.StreamWriter file;
+        string folderName;
+        string folderPath;
+        string package;
+
+        public Form1()
+        {
+            String tmp = Directory.GetCurrentDirectory();
+            folderName = tmp + "\\Packages";
+            if (!Directory.Exists(folderName)) System.IO.Directory.CreateDirectory(folderName);
+            InitializeComponent();
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public void startFileWatchers()
+        {
+            package = textBox3.Text;
+            folderPath = System.IO.Path.Combine(folderName, package);
+            System.IO.Directory.CreateDirectory(folderPath);
+            if (checkBox1.Checked)
+            {
+                // Create a new FileSystemWatcher and set its properties.
+                watcher = new FileSystemWatcher();
+                watcher.Path = "C:\\";
+                /* Watch for changes in LastAccess and LastWrite times, and
+                   the renaming of files or directories. */
+                watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                   | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                // Only watch text files.
+                //watcher.Filter = "*.txt";
+                watcher.IncludeSubdirectories = true;
+
+                // Add event handlers.
+                watcher.Changed += new FileSystemEventHandler(OnChanged);
+                watcher.Created += new FileSystemEventHandler(OnChanged);
+                watcher.Deleted += new FileSystemEventHandler(OnChanged);
+                watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+                // Begin watching.
+                watcher.EnableRaisingEvents = true; 
+            }
+
+            if (checkBox2.Checked)
+            {
+                watcher2 = new FileSystemWatcher();
+                watcher2.Path = "D:\\";
+                /* Watch for changes in LastAccess and LastWrite times, and
+                   the renaming of files or directories. */
+                watcher2.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                   | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                // Only watch text files.
+                //watcher2.Filter = "*.txt";
+                watcher2.IncludeSubdirectories = true;
+
+                // Add event handlers.
+                watcher2.Changed += new FileSystemEventHandler(OnChanged);
+                watcher2.Created += new FileSystemEventHandler(OnChanged);
+                watcher2.Deleted += new FileSystemEventHandler(OnChanged);
+                watcher2.Renamed += new RenamedEventHandler(OnRenamed);
+
+                // Begin watching.
+                watcher2.EnableRaisingEvents = true; 
+            }
+        }
+
+        // Define the event handlers. 
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+
+            string[] words = e.FullPath.Split('\\');
+            writeToKonzole("File: " + e.FullPath + " " + e.ChangeType + Environment.NewLine);
+            if (e.ChangeType == System.IO.WatcherChangeTypes.Created)
+            {
+                try
+                {
+                    if (!words[words.Length - 1].Equals(package + ".txt") && !words[words.Length - 1].Equals("before.reg") && !words[words.Length - 1].Equals("after.reg") && File.Exists(e.FullPath)) file.WriteLine(e.FullPath);
+                }
+                catch (IOException)
+                {
+
+                }
+            }
+            if (e.ChangeType == System.IO.WatcherChangeTypes.Changed)
+            {
+                if (File.Exists(e.FullPath))
+                {
+                    bool nasiel = false;
+                    string[] readText = File.ReadAllLines(folderPath + "\\" + package + ".txt");
+                    foreach (string s in readText) {
+                        if (s.Equals(e.FullPath)) nasiel = true;
+                    }
+                    try
+                    {
+                        if (!words[words.Length - 1].Equals(package + ".txt") && !words[words.Length - 1].Equals("before.reg") && !words[words.Length - 1].Equals("after.reg") && !nasiel) file.WriteLine(e.FullPath);
+                    }
+                    catch (IOException)
+                    {
+
+                    }
+                }    
+            }
+        }
+
+        private void copyTrackedFiles()
+        {
+            string[] lines = System.IO.File.ReadAllLines(folderPath + "\\" + package + ".txt");
+            foreach (string line in lines)
+            {
+                string newPath = line.Substring(3);
+                if (File.Exists(line)) { 
+                    if (!Directory.Exists(Path.GetDirectoryName(System.IO.Path.Combine(folderPath, newPath))))
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(System.IO.Path.Combine(folderPath, newPath)));
+                    }
+                    System.IO.File.Copy(line, System.IO.Path.Combine(folderPath, newPath), true);
+                }
+            }
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Specify what is done when a file is renamed.
+            writeToKonzole("File: " + e.OldFullPath + " renamed to " + e.FullPath + Environment.NewLine);
+            if (File.Exists(e.FullPath))
+            {
+                file.Close();
+                string[] readText = File.ReadAllLines(folderPath + "\\" + package + ".txt");
+                for (int i = 0; i < readText.Length; i++)
+                {
+                    if (readText[i].Equals(e.OldFullPath))
+                    {
+                        readText[i] = e.FullPath;
+                    }
+                    writeToKonzole(readText[i] + Environment.NewLine);
+                }
+                File.WriteAllLines(folderPath + "\\" + package + ".txt", readText);    
+                file = new System.IO.StreamWriter(folderPath + "\\" + package + ".txt", true);
+            }
+        }
+
+        public void ExportKey(string RegKey, string SavePath)
+        {
+            string path = "\"" + SavePath + "\"";
+            string key = "\"" + RegKey + "\"";
+
+            var proc = new Process();
+            try
+            {
+                proc.StartInfo.FileName = "regedit.exe";
+                proc.StartInfo.UseShellExecute = false;
+                proc = Process.Start("regedit.exe", "/e " + path + " " + key + "");
+
+                if (proc != null) proc.WaitForExit();
+            }
+            finally
+            {
+                if (proc != null) proc.Dispose();
+            }
+
+        }
+
+        public void ImportKey(string SavePath)
+        {
+            string path = "\"" + SavePath + "\"";
+
+            var proc = new Process();
+            try
+            {
+                proc.StartInfo.FileName = "regedit.exe";
+                proc.StartInfo.UseShellExecute = false;
+                proc = Process.Start("regedit.exe", path + "");
+
+                if (proc != null) proc.WaitForExit();
+            }
+            finally
+            {
+                if (proc != null) proc.Dispose();
+            }
+
+        }
+
+        private string[] getResults(DiffList_TextFile destination, ArrayList DiffLines)
+        {
+            int i, j=1;
+            string[] res = new string[4000];
+            res[0] = "Windows Registry Editor Version 5.00";
+            foreach (DiffResultSpan drs in DiffLines)
+            {
+                switch (drs.Status)
+                {
+                    case DiffResultSpanStatus.AddDestination:
+                        for (i = 0; i < drs.Length; i++)
+                        {
+                            res[j] = ((TextLine)destination.GetByIndex(drs.DestIndex + i)).Line.ToString();
+                            j++;
+                        }
+                        break;
+                }
+            }
+            return res;
+        }
+
+        private void TextDiff(string sFile, string dFile)
+        {
+            DiffList_TextFile sLF = null;
+            DiffList_TextFile dLF = null;
+            try
+            {
+                sLF = new DiffList_TextFile(sFile);
+                dLF = new DiffList_TextFile(dFile);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "File Error");
+                return;
+            }
+
+            try
+            {
+                double time = 0;
+                DiffEngine de = new DiffEngine();
+                time = de.ProcessDiff(sLF, dLF, DiffEngineLevel.FastImperfect);
+                ArrayList rep = de.DiffReport();
+                File.WriteAllLines(folderPath + "\\" + package + ".reg", getResults(dLF, rep));
+            }
+            catch (Exception ex)
+            {
+                string tmp = string.Format("{0}{1}{1}***STACK***{1}{2}",
+                    ex.Message,
+                    Environment.NewLine,
+                    ex.StackTrace);
+                MessageBox.Show(tmp, "Compare Error");
+                return;
+            }
+        }
+
+        private void writeToKonzole(string s)
+        {
+            textBox1.BeginInvoke((MethodInvoker)(() => textBox1.AppendText(s)));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button3.Enabled = false;
+            startFileWatchers();
+            file = new System.IO.StreamWriter(folderPath + "\\" + package + ".txt", true);
+            ExportKey("HKEY_CURRENT_USER\\SOFTWARE", folderPath + "\\before.reg");
+            writeToKonzole("Registre exportnute" + Environment.NewLine);
+            button2.Enabled = true;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            file.Close();
+            button2.Enabled = false;
+            if (watcher != null) watcher.EnableRaisingEvents = false;
+            if (watcher2 != null) watcher2.EnableRaisingEvents = false;
+            copyTrackedFiles();
+            writeToKonzole("Subory odkopirovane" + Environment.NewLine);
+            ExportKey("HKEY_CURRENT_USER\\SOFTWARE", folderPath + "\\after.reg");
+            writeToKonzole("Registre exportnute" + Environment.NewLine);
+            TextDiff(folderPath + "\\before.reg", folderPath + "\\after.reg");
+            writeToKonzole("Registre diffnute" + Environment.NewLine);
+            File.Delete(folderPath + "\\after.reg");
+            File.Delete(folderPath + "\\before.reg");
+            button1.Enabled = true;
+            getPackages();
+            writeToKonzole(package + " zaznamenany" + Environment.NewLine);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button3.Enabled = false;
+            package = (string)comboBox1.SelectedItem;
+            folderPath = System.IO.Path.Combine(folderName, package);
+            if (System.IO.File.Exists(System.IO.Path.Combine(folderPath, package + ".txt")))
+            {
+                string[] lines = System.IO.File.ReadAllLines(System.IO.Path.Combine(folderPath, package + ".txt"));
+                foreach (string line in lines)
+                {
+                    string newPath = line.Substring(3);
+                    if (File.Exists(System.IO.Path.Combine(folderPath, newPath))) { 
+                        if (!Directory.Exists(Path.GetDirectoryName(line)))
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(line));
+                        }
+                        System.IO.File.Copy(System.IO.Path.Combine(folderPath, newPath), line, true);
+                    }
+                }
+                ImportKey(System.IO.Path.Combine(folderPath, package + ".reg"));
+                writeToKonzole(package + " nainstalovany" + Environment.NewLine);
+            }
+            else
+            {
+                writeToKonzole("Nenasiel som instalaciu pre " + package + Environment.NewLine);
+            }
+            button1.Enabled = true;
+            button3.Enabled = true;
+        }
+
+        private void getPackages()
+        {
+            comboBox1.Items.Clear();
+            string[] words;
+            string[] subdirectoryEntries = Directory.GetDirectories(folderName);
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                words = subdirectory.Split('\\');
+                comboBox1.Items.Add(words[words.Length - 1]);
+            }
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+                button3.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getPackages();
+        }
+    }
+}
